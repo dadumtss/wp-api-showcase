63aa22ae20a3b343e599c966876c92adb48415fe
dadumtss <dadumtss@something.com>
2018-06-01 19:04:33 +0200

    TASK: show everything

--- php/index.php
+++ php/index.php
@@ -1,5 +1,25 @@
 <?php
 
+use MYTHEME\Greeting;
+use MYTHEME\Login;
+
+
+session_start();
+
 require_once 'functions.php';
 
-var_dump(getPosts());
+$Greetings = new Greeting();
+$Login = new Login();
+
+if (isset($_GET['logout']) && $_GET['logout'] === '1') {
+	$Login->logout();
+}
+
+if ($Login->checkLogin()) {
+	echo '<p><a href="?logout=1">Logout</a></p>';
+	$Greetings->displayForm();
+	$Greetings->displayGreetings();
+} else {
+	$Login->displayLoginForm();
+}
+
